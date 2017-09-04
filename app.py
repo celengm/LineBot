5186dad5d0e77b78f71a16512ac81f350ea787fd
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-09-05 04:59:34 +0800

    output hex of image

--- app.py
+++ app.py
@@ -509,11 +509,19 @@ def handle_content_message(event):
             dest_name = os.path.basename(dest_path)
             os.rename(tempfile_path, dest_path)
 
-            imgur_url = imgur_api.upload(dest_path)
+            # imgur_url = imgur_api.upload(dest_path)
+
+            import binascii
+
+            with open(dest_path, 'rb') as f:
+                hexdata = binascii.hexlify(f.read())
+            
+            hexlist = map(' '.join, zip(*[iter(hexdata)]*2))
 
         os.remove(dest_path)
 
-        api_reply(token, TextSendMessage(text=u'檔案已上傳至imgur。\nURL: {}'.format(imgur_url)), src)
+        # api_reply(token, TextSendMessage(text=u'檔案已上傳至imgur。\nURL: {}'.format(imgur_url)), src)
+        api_reply(token, TextSendMessage(text=hexlist, src)
     except ImgurClientError as e:
         text = u'開機時間: {}\n\n'.format(sys_data.boot_up)
         text += u'Imgur API發生錯誤，狀態碼: {}\n\n錯誤訊息: {}'.format(e.status_code, e.error_message)
