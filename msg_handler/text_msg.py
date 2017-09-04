4308222d8ecef34154e9f388d8ee530e2c15d801
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-09-04 22:45:22 +0800

    debug

--- msg_handler/text_msg.py
+++ msg_handler/text_msg.py
@@ -138,6 +138,7 @@ class text_msg(object):
                 text = error.main.lack_of_thing(u'參數')
 
             if results is not None:
+                print results
                 if isinstance(results, str):
                     text = results
                 else:
