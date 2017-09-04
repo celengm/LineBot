2460d0c54313358ee5b7f5eb6532482456e68dbd
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-09-04 22:24:15 +0800

    change - reply splitter ' ' to '  '

--- db/kwdict.py
+++ db/kwdict.py
@@ -388,7 +388,7 @@ class kw_dict_mgr(object):
         """
         from msg_handler.text_msg import split
 
-        reply_splitter = ' '
+        reply_splitter = '  '
         split_iter = split(reply_text_in_db, reply_splitter, 2)
 
         return {'main': split_iter[0], 'attached': split_iter[1]}
