0da6beaabe1f15943221023e6310b37faeaa16fd
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-09-10 20:07:01 +0800

    debug - background worker

--- app.py
+++ app.py
@@ -414,7 +414,7 @@ def handle_text_message(event):
                 api_reply(token, TextSendMessage(text=text), src)
                 return
             else:
-                calc_result = q.enqueue(txt_calc.text_calculator.calc, zip(text, sys_data.calc_debug))
+                calc_result = q.enqueue(txt_calc.text_calculator.calc, (text, sys_data.calc_debug))
                 # calc_result = txt_calc.text_calculator.calc(text, sys_data.calc_debug)
                 if calc_result is not None:
                     sys_data.helper_cmd_dict['CALC'].count += 1
