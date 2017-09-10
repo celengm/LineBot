9bce1b0ac39e4e978499b6f8556af71a6bbfc616
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-09-11 00:47:06 +0800

    debug - modulize

--- db/db_base.py
+++ db/db_base.py
@@ -6,10 +6,7 @@ from sqlalchemy.exc import IntegrityError
 
 class db_base_obj(object):
     def __init__(self, db_query_mgr):
-        urlparse.uses_netloc.append(scheme)
-        self.url = urlparse.urlparse(db_url)
         self.db_query_mgr = db_query_mgr
-        self._set_connection()
 
     def sql_cmd_only(self, cmd):
         return self.db_query_mgr.sql_cmd_only(cmd)
