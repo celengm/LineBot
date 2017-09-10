557c0907748c5304e5c3e7b8342e62deb5dff61c
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-09-10 16:30:56 +0800

    debug - image handle

--- msg_handler/img_msg.py
+++ msg_handler/img_msg.py
@@ -2,6 +2,7 @@
 
 import os
 import tempfile
+import hashlib
 
 class img_msg(object):
     def __init__(self, line_api, imgur_api, tmp_path):
@@ -12,19 +13,9 @@ class img_msg(object):
 
     def image_handle(self, line_msg):
         message_content = self._line_api.get_content(line_msg.id)
+        sha224 = hashlib.sha224(message_content.iter_content(4096)[0]).hexdigest()
 
-        with tempfile.NamedTemporaryFile(dir=self.tmp_path, delete=False) as tf:
-            for chunk in message_content.iter_content():
-                tf.write(chunk)
-            tempfile_path = tf.name
-
-            dest_path = tempfile_path + '.jpg'
-            dest_name = os.path.basename(dest_path)
-            os.rename(tempfile_path, dest_path)
-
-        os.remove(dest_path)
-
-        return u'GET'
+        return u'File Checksum (SHA224):\n{}'.format(sha224)
 
     def upload_imgur(self, line_msg):
         message_content = self._line_api.get_content(line_msg.id)
