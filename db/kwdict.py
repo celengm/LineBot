fca1129ab9cfbed878267438b02d68d9f4dd528e
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-09-10 18:12:58 +0800

    debug - reply determining error

--- db/kwdict.py
+++ db/kwdict.py
@@ -63,7 +63,10 @@ class kw_dict_mgr(object):
         keyword = keyword.replace('\\', '\\\\').replace(r'\\n', '\n')
         reply = reply.replace('\\', '\\\\').replace(r'\\n', '\n')
 
-        is_illegal_reply_attachment = lambda reply_obj: {'is_legal': reply_obj['attachment'] is not None and len(reply_obj['attachment']) <= 50, 
+        is_illegal_reply_attachment = lambda reply_obj: {'is_legal': reply_obj['attachment'] is None or
+                                                                     (reply_obj['attachment'] is not None 
+                                                                     and 
+                                                                     len(reply_obj['attachment']) <= 50), 
                                                          'test_reply_object': reply_obj}
         illegal_reply_object_test = is_illegal_reply_attachment(kw_dict_mgr.split_reply(reply, is_pic_reply))
 
