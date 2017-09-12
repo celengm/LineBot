edb7087eb7f78f59dfbaee744b584ec7c44e35a4
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-09-12 17:06:53 +0800

    debug - sql queue

--- bot/sql.py
+++ bot/sql.py
@@ -4,6 +4,8 @@ import urlparse
 import psycopg2
 from sqlalchemy.exc import IntegrityError
 import traceback
+import Queue
+from threading import Thread
 
 from bot import webpage_auto_gen 
 
@@ -14,11 +16,24 @@ class db_query_manager(object):
         self.url = urlparse.urlparse(db_url)
         self.set_connection()
         self._auto_gen = webpage_auto_gen.webpage(flask_app)
+        self.work_queue = Queue.Queue()
 
     def sql_cmd_only(self, cmd):
         return self.sql_cmd(cmd, None)
 
     def sql_cmd(self, cmd, dict):
+        self.work_queue.put(sql_query_obj(cmd, dict))
+        sql_thread = threadWithReturn(target=self._query_worker)
+        sql_thread.daemon = True
+        sql_thread.run()
+        return sql_thread.join()
+
+    def _query_worker(self):
+        query_obj = self.work_queue.get()
+
+        cmd = query_obj.cmd
+        dict = query_obj.dict
+
         try:
             self.cur.execute(cmd, dict)
             result = self.cur.fetchall()
@@ -29,6 +44,7 @@ class db_query_manager(object):
             elif ex.message == 'can\'t execute an empty query':
                 result = None
             else:
+                self.work_queue.task_done()
                 raise ex
         except psycopg2.InternalError as uiex:
             text = uiex.message
@@ -49,8 +65,10 @@ class db_query_manager(object):
             result = None
 
             self._auto_gen.rec_error(text, traceback.format_exc().decode('utf-8'), u'(SQL DB)')
+            self.work_queue.task_done()
             raise e
         
+        self.work_queue.task_done()
         if result is not None:
             if len(result) > 0:
                 return result
@@ -72,6 +90,36 @@ class db_query_manager(object):
             port=self.url.port
         )
         self.cur = self.conn.cursor()
+
+
+class sql_query_obj(object):
+    def __init__(self, cmd, dict):
+        self.cmd = cmd
+        self.dict = dict
+
+    @property
+    def cmd(self):
+        return self.cmd
+    
+    @property
+    def dict(self):
+        return self.dict
+
+
+class threadWithReturn(Thread):
+    def __init__(self, *args, **kwargs):
+        super(threadWithReturn, self).__init__(*args, **kwargs)
+
+        self._return = None
+
+    def run(self):
+        if self._Thread__target is not None:
+            self._return = self._Thread__target(*self._Thread__args, **self._Thread__kwargs)
+
+    def join(self, *args, **kwargs):
+        super(threadWithReturn, self).join(*args, **kwargs)
+
+        return self._return
 
 
 
