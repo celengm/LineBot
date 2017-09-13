f07e23c8db7a4b2d82ac61c379dfbd55a8dee03f
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-09-14 03:18:43 +0800

    debug - latex render

--- app.py
+++ app.py
@@ -408,7 +408,7 @@ def handle_text_message(event):
 
                 if calc_result.latex_avaliable:
                     text += u'\nLaTeX URL:\n{}'.format(webpage_generator.rec_latex(calc_result.latex))
-                    print webpage_generator._page_content[self._latex_route]
+                    print webpage_generator._page_content[webpage_generator._latex_route]
 
                 api_reply(token, TextSendMessage(text=text), src)
 
