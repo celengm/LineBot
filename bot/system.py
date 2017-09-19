0a6f93b780367b3ebbee77d7cbd1aee565172606
RaenonX JELLYCAT <maplestory0710@gmail.com>
2017-09-19 23:34:27 +0800

    debug - imgur upload

--- bot/system.py
+++ bot/system.py
@@ -241,7 +241,7 @@ class imgur_proc(object):
 	        }
             return self._imgur_api.upload_from_path(path, config=config, anon=False)['link']
         except ImgurClientError as e:
-            text += u'Imgur API發生錯誤，狀態碼: {}\n\n錯誤訊息: {}'.format(e.status_code, e.error_message)
+            text = u'Imgur API發生錯誤，狀態碼: {}\n\n錯誤訊息: {}'.format(e.status_code, e.error_message)
 
             return text
 
